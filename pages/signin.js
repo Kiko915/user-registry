e96f35c541cdf15534aae88530b319a04ddadaab
Francis Kiko <francismistica06@gmail.com>
2022-03-06 22:53:49 +0800

    Changes in routes

--- pages/signin.js
+++ pages/signin.js
@@ -1,22 +1,32 @@
-import {Button, Divider, Typography} from "antd";
-import { GithubOutlined } from '@ant-design/icons';
-import styles from '../styles/Home.module.css'
+import { Button, Divider, Typography } from "antd";
+import { GithubOutlined } from "@ant-design/icons";
+import styles from "../styles/Home.module.css";
 import Head from "next/head";
-import {signIn} from "next-auth/react";
+import { signIn } from "next-auth/react";
 const { Title, Text } = Typography;
 
 export default function SignIn() {
-    return (
-        <>
-        <Head>
-            <title>Sign In | User Registry</title>
-        </Head>
-        <main className={styles.login} >
-            <Title>Sign In</Title>
-            <Text>Continue with github</Text>
-            <Divider />
-            <Button type='primary' icon={<GithubOutlined />} onClick={() => signIn('github', { callbackUrl: 'http://localhost:3000/' })} >Github</Button>
-        </main>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <title>Sign In | User Registry</title>
+      </Head>
+      <main className={styles.login}>
+        <Title>Sign In</Title>
+        <Text>Continue with github</Text>
+        <Divider />
+        <Button
+          type="primary"
+          icon={<GithubOutlined />}
+          onClick={() =>
+            signIn("github", {
+              callbackUrl: "https://user-registry.vercel.app/",
+            })
+          }
+        >
+          Github
+        </Button>
+      </main>
+    </>
+  );
 }
